5ec3587863230469d91b008dc30b904fc51e0778
Gaby Campos <gabycamposcontact@gmail.com>
2023-02-28 15:34:33 -0800

    testing routing after login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FaveParks from './components/FaveParks';
-import { Home } from './components/Home'
-import { Nav } from './components/Nav'
-import { ParkList } from './components/ParkList'
-import { IndivPark } from './components/IndivPark'
-import { About } from './components/About'
-import { Search } from './components/Search'
+import { Home } from './components/Home';
+import { Nav } from './components/Nav';
+import { ParkList } from './components/ParkList';
+import { IndivPark } from './components/IndivPark';
+import { About } from './components/About';
+import { Search } from './components/Search';
+import { useAuth0 } from '@auth0/auth0-react';
+import Loading from './components/Loading';
+
 
 function App() {
+  
+  const { user } = useAuth0();
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />
+  }
+  
   return (
     <div className="App">
       <Router>
         <Nav/>
+        {!user ? <span>NOT A USER!!!</span> : <span>Hello user <Link to="my-list">{user}</Link></span>}
         <Routes>
           <Route exact path='/' element={<Home/>}>
             <Route
